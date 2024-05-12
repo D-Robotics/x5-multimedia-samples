238447be5c04377ada3a71ead0c5bb3bd41604c9
yaqiang.li <yaqiang.li@horizon.cc>
2024-05-12 09:28:36 +0800

    feat(vp_sensor): [X5-440] Add f37 sensor config
    Summary:
    1. Add f37 sensor config to vp sensor list.
    2. Sample can access f37 sensor.
    
    Signed-off-by: zilei.li <zilei.li@horizon.cc>
    Change-Id: I79d6f2ba09cffdca56c46e7aa55b310192343399
    Reviewed-by: yaqiang.li <yaqiang.li@horizon.cc>
    Tested-by: yaqiang.li <yaqiang.li@horizon.cc>
    CI: yaqiang.li <yaqiang.li@horizon.cc>

--- vp_sensors/vp_sensors.c
+++ vp_sensors/vp_sensors.c
@@ -19,6 +19,7 @@ extern vp_sensor_config_t sc132gs_linear_1088x1280_raw10_10fps_2lane;
 extern vp_sensor_config_t sc132gs_linear_896x896_raw10_10fps_2lane;
 extern vp_sensor_config_t sc035hgs_linear_640x480_raw10_30fps_1lane;
 extern vp_sensor_config_t ov5640_linear_1920x1080_raw10_30fps_2lane;
+extern vp_sensor_config_t f37_linear_1920x1080_raw10_30fps_1lane;
 
 vp_sensor_config_t *vp_sensor_config_list[] = {
 	&sc1330t_linear_1280x960_raw10_30fps_1lane,
@@ -28,7 +29,8 @@ vp_sensor_config_t *vp_sensor_config_list[] = {
 	&sc132gs_linear_1088x1280_raw10_10fps_2lane,
 	&sc132gs_linear_896x896_raw10_10fps_2lane,
 	&sc035hgs_linear_640x480_raw10_30fps_1lane,
-	&ov5640_linear_1920x1080_raw10_30fps_2lane
+	&ov5640_linear_1920x1080_raw10_30fps_2lane,
+	&f37_linear_1920x1080_raw10_30fps_1lane
 };
 
 uint32_t vp_get_sensors_list_number() {

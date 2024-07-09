914feb6e2ea37722d83bcd23944d6408408b0feb
yaqiang.li <yaqiang.li@d-robotics.cc>
2024-07-09 10:22:53 +0800

    feat(vp_sensor|irs2381c): [X5-1139] Support ToF irs2381c
    Sensor parameters:
    
        MCLK: 24MHz
        Resolution: 224 x 173 x 11 = 224 x 1903
        Frame rate: 5fps
        2 lane, 12bit raw
    
    Signed-off-by: yaqiang.li <yaqiang.li@d-robotics.cc>
    Change-Id: If0b808545ace289f4bd72875de9b4a5618e6220c

--- vp_sensors/vp_sensors.c
+++ vp_sensors/vp_sensors.c
@@ -20,12 +20,12 @@ extern vp_sensor_config_t irs2875_linear_208x1413_raw12_15fps_2lane;
 extern vp_sensor_config_t sc230ai_linear_1920x1080_raw10_10fps_1lane;
 extern vp_sensor_config_t sc230ai_linear_1920x1080_raw10_30fps_1lane;
 extern vp_sensor_config_t sc132gs_linear_1088x1280_raw10_30fps_1lane;
-//extern vp_sensor_config_t sc132gs_linear_896x896_raw10_10fps_2lane;
 extern vp_sensor_config_t sc035hgs_linear_640x480_raw10_30fps_1lane;
 extern vp_sensor_config_t ov5640_linear_1920x1080_raw10_30fps_2lane;
 extern vp_sensor_config_t f37_linear_1920x1080_raw10_30fps_1lane;
 extern vp_sensor_config_t imx415_linear_3480x2160_raw10_30fps_4lane;
 extern vp_sensor_config_t sc202cs_linear_1600x1200_raw10_30fps_1lane;
+extern vp_sensor_config_t irs2381c_linear_224x1903_raw12_5fps_2lane;
 
 vp_sensor_config_t *vp_sensor_config_list[] = {
 	&sc1330t_linear_1280x960_raw10_30fps_1lane,
@@ -38,6 +38,7 @@ vp_sensor_config_t *vp_sensor_config_list[] = {
 	&f37_linear_1920x1080_raw10_30fps_1lane,
 	&imx415_linear_3480x2160_raw10_30fps_4lane,
 	&sc202cs_linear_1600x1200_raw10_30fps_1lane,
+	&irs2381c_linear_224x1903_raw12_5fps_2lane,
 };
 
 uint32_t vp_get_sensors_list_number() {

fc55a5e4f2ca7898b2d4ef06810ba6608b14c4e6
qinlin.wu <qinlin.wu@horizon.cc>
2024-09-25 11:18:41 +0800

    feat(app): [X5-1856] Add print system timestamp [3/3]
    Change-Id: I4e2d7ee0b29244e660398e13ba211fe483997a71
    Signed-off-by: qinlin.wu <qinlin.wu@horizon.cc>

--- sample_vse/sample_vse.c
+++ sample_vse/sample_vse.c
@@ -143,6 +143,7 @@ void vp_vin_print_hbn_vnode_image_t(const hbn_vnode_image_t *frame)
 void vp_vin_print_hbn_frame_info_t(const hbn_frame_info_t *frame_info) {
 	printf("Frame ID: %u\n", frame_info->frame_id);
 	printf("Timestamps: %lu\n", frame_info->timestamps);
+	printf("Systimestamps: %lu\n", frame_info->sys_timestamps);
 	printf("tv: %ld.%06ld\n", frame_info->tv.tv_sec, frame_info->tv.tv_usec);
 	printf("trig_tv: %ld.%06ld\n", frame_info->trig_tv.tv_sec, frame_info->trig_tv.tv_usec);
 	printf("Frame Done: %u\n", frame_info->frame_done);

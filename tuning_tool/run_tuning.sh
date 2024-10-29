20de114bb657511aa46e90d2fb364209089d6a33
jinbao.he <jinbao.he@d-robotics.cc>
2024-10-29 17:44:25 +0800

    feat(tuning_tool): [X5-1476] Add os08c10 tuning_tool config
    Signed-off-by: jinbao.he <jinbao.he@d-robotics.cc>
    Change-Id: Icc04924fba75da1882c7c35e11a5ba0ef2759f9b

--- tuning_tool/run_tuning.sh
+++ tuning_tool/run_tuning.sh
@@ -98,6 +98,10 @@ function suit_case_run() {
 		echo "Run $run_case"
 		${COMMON_DIR}/isp_tuning -v "${vpm_json_path}" -c "${cam_json_path}" "$@"
 
+	elif test "$run_case" == "os08c10_rx0"; then
+		echo "Run $run_case"
+		${COMMON_DIR}/isp_tuning -v "${vpm_json_path}" -c "${cam_json_path}" "$@"
+
 	elif test "$run_case" == "feedback_case"; then
 		echo "Run $run_case"
 		${COMMON_DIR}/isp_tuning -v "${vpm_json_path}" -c "${cam_json_path}" -w 1 "$@"

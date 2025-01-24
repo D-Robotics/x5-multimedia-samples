c6ae1f7333467e4aa0e408a0d8a406aa60c7bcfc
xiangqing.shi <xiangqing.shi@d-robotics.cc>
2025-01-24 22:01:34 +0800

    feat(sysinfopro): [X5-2429] Add sysinfopro tool to retrieve system information
    Summary:
    - The newly added sysinfopro tool can obtain various system information,
      including Hardware Model, SDK Version, Kernel Version, Uboot Version,
      BPU HW_IO Git Commit Hash, CPU, DDR, GPU, and BPU Status, Memory Info,
      ION Memory Info, Board Info, HBRE lib version, and Kernel Module List.
    - Execute the "./sysinfopro -h" command in the
      /app/platform_samples/sysinfopro
      directory to get detailed instructions.
    
    Signed-off-by: xiangqing.shi <xiangqing.shi@horizon.cc>
    Change-Id: I2038861ea2b35bbe2666bd5c9225aafea60afc7a

---
+++ sysinfopro/sysinfopro.h
@@ -0,0 +1,22 @@
+#ifndef SYSINFOPRO_H
+#define SYSINFOPRO_H
+
+#include "hb_camera_interface.h"
+#include "codec_cfg.h"
+#include "hb_dsp_mgr.h"
+#include "boardinfo.h"
+#include "drobot_efuse.h"
+#include "hb_mem_pc_alloc.h"
+#include "ipcf_hal_data.h"
+#include "logd.h"
+#include "hb_media_codec.h"
+#include "drobot_secure_storage.h"
+#include "te600_engine.h"
+#include "hobot_ota_hl.h"
+#include "hb_powerctl_out_version.h"
+#include "hb_tool_server.h"
+#include "nano2D.h"
+#include "mem_list.h"
+#include "gc_hal_version.h"
+
+#endif // SYSINFOPRO_H

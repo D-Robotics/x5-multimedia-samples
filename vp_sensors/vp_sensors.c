196548e494ca0b8122752a3963d75e8327a2d786
jinbao.he <jinbao.he@d-robotics.cc>
2025-04-14 12:15:51 +0800

    feat(vp_sensor): [X5-3028] Add ov9782 sensor config
    Summary:
    1. Add ov9782 sensor config to vp sensor list.
    2. Sample can access ov9782 sensor.
    
    Change-Id: I74e82586c8a88a49bb195bf62ff8aa6af3f3fe76
    Signed-off-by: jinbao.he <jinbao.he@d-robotics.cc>

--- vp_sensors/vp_sensors.c
+++ vp_sensors/vp_sensors.c
@@ -39,6 +39,8 @@ extern vp_sensor_config_t sc1336_linear_1280x720_raw10_15fps_2lane;
 extern vp_sensor_config_t dummy_sensor_config;
 extern vp_sensor_config_t ar0233_linear_1920x1080_raw12_30fps_2lane_vc0;
 extern vp_sensor_config_t ar0233_linear_1920x1080_raw12_30fps_2lane_vc1;
+extern vp_sensor_config_t ov9782_linear_640x360_raw10_200fps_2lane;
+extern vp_sensor_config_t ov9782_linear_1280x720_raw10_120fps_2lane;
 
 vp_sensor_config_t *vp_sensor_config_list[] = {
 	&sc1330t_linear_1280x960_raw10_30fps_1lane,
@@ -65,6 +67,8 @@ vp_sensor_config_t *vp_sensor_config_list[] = {
 	&dummy_sensor_config,
 	&ar0233_linear_1920x1080_raw12_30fps_2lane_vc0,
 	&ar0233_linear_1920x1080_raw12_30fps_2lane_vc1,
+	&ov9782_linear_640x360_raw10_200fps_2lane,
+	&ov9782_linear_1280x720_raw10_120fps_2lane,
 
 };
 

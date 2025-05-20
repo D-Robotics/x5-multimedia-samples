90ca3a06e7cb355062d4f9379089a064940ed914
xin03.liu <xin03.liu@d-robotics.cc>
2025-05-20 21:07:06 +0800

    fix(sample): [X5-3122] Not config mclk, should skpi hbn_vnode_set_attr_ex()
    Signed-off-by: xin03.liu <xin03.liu@d-robotics.cc>
    Change-Id: I4da55462d9091d118beecd2754ee6d9c1ec17c07

--- tuning_tool/tuning_tool.c
+++ tuning_tool/tuning_tool.c
@@ -692,6 +692,7 @@ static int32_t create_vin_node(pipe_contex_t *pipe_contex, uint32_t pipelinemode
 	}else{
 		vin_attr_ex.vin_attr_ex_mask = sensor_config->vin_attr_ex->vin_attr_ex_mask;
 		vin_attr_ex.mclk_ex_attr.mclk_freq = sensor_config->vin_attr_ex->mclk_ex_attr.mclk_freq;
+		vin_attr_ex_mask = vin_attr_ex.vin_attr_ex_mask;
 	}
 
 	FUNC_EQ(hbn_vnode_open(HB_VIN, hw_id, AUTO_ALLOC_ID, vin_node_handle), 0, return RET_FAILURE);
@@ -699,7 +700,6 @@ static int32_t create_vin_node(pipe_contex_t *pipe_contex, uint32_t pipelinemode
 	FUNC_EQ(hbn_vnode_set_ichn_attr(*vin_node_handle, ichn_id, vin_ichn_attr), 0, return RET_FAILURE);
 	FUNC_EQ(hbn_vnode_set_ochn_attr(*vin_node_handle, ochn_id, vin_ochn_attr), 0, return RET_FAILURE);
 
-	vin_attr_ex_mask = vin_attr_ex.vin_attr_ex_mask;
 	if (vin_attr_ex_mask) {
 		for (uint8_t i = 0; i < VIN_ATTR_EX_INVALID; i ++) {
 			if ((vin_attr_ex_mask & (1 << i)) == 0)

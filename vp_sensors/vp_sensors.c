2713ff8764d28a4254c938a3be4de881b274c303
fuhua.wang <fuhua.wang@d-robotics.cc>
2025-09-17 22:38:02 +0800

    feat(sample): [X5-2942] Support sc230ai dol2
    Summary:
    - Add dol2 support for sc230ai in vpsensor
    
    Signed-off-by: fuhua.wang <fuhua.wang@d-robotics.cc>
    Change-Id: Ifaab7a8b143c36d14e169d58aab7683d77909e18

--- vp_sensors/vp_sensors.c
+++ vp_sensors/vp_sensors.c
@@ -19,6 +19,7 @@ extern vp_sensor_config_t sc1330t_linear_1280x960_raw10_30fps_1lane;
 extern vp_sensor_config_t irs2875_linear_208x1413_raw12_15fps_2lane;
 extern vp_sensor_config_t sc230ai_linear_1920x1080_raw10_10fps_1lane;
 extern vp_sensor_config_t sc230ai_linear_1920x1080_raw10_30fps_1lane;
+extern vp_sensor_config_t sc230ai_dol2_1920x1080_raw10_30fps_2lane;
 extern vp_sensor_config_t sc132gs_linear_1088x1280_raw10_60fps_1lane;
 extern vp_sensor_config_t sc132gs_hdr_1088x1280_raw10_30fps_2lane;
 extern vp_sensor_config_t sc035hgs_linear_640x480_raw10_30fps_1lane;
@@ -64,6 +65,7 @@ vp_sensor_config_t *vp_sensor_config_list[] = {
 	&irs2875_linear_208x1413_raw12_15fps_2lane,
 	&sc230ai_linear_1920x1080_raw10_10fps_1lane,
 	&sc230ai_linear_1920x1080_raw10_30fps_1lane,
+	&sc230ai_dol2_1920x1080_raw10_30fps_2lane,
 	&sc132gs_linear_1088x1280_raw10_60fps_1lane,
 	&sc132gs_hdr_1088x1280_raw10_30fps_2lane,
 	&sc035hgs_linear_640x480_raw10_30fps_1lane,

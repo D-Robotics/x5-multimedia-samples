5490407cf6cc658ebbe92dfe21fd850489c4b8fa
pengjun.xie <pengjun.xie@d-robotics.cc>
2025-09-19 10:48:26 +0800

    feat(vp_sensor): [X5-3600] Add sc850sl sensor config
    Signed-off-by: pengjun.xie <pengjun.xie@d-robotics.cc>
    Change-Id: Idf72a480c846fd5cde254ee67754b3093d90da26

--- vp_sensors/vp_sensors.c
+++ vp_sensors/vp_sensors.c
@@ -60,6 +60,7 @@ extern vp_sensor_config_t ov50h40_linear_4096x3072_raw10_30fps_4lane;
 extern vp_sensor_config_t ox05b1s_linear_2592x1944_raw10_30fps_4lane;
 extern vp_sensor_config_t ox05b1s_linear_2592x1944_raw10_10fps_2lane;
 extern vp_sensor_config_t imx415_linear_3480x2160_raw10_60fps_4lane;
+extern vp_sensor_config_t sc850sl_linear_3840x2160_raw10_30fps_4lane;
 
 vp_sensor_config_t *vp_sensor_config_list[] = {
 	&sc1330t_linear_1280x960_raw10_30fps_1lane,
@@ -107,6 +108,7 @@ vp_sensor_config_t *vp_sensor_config_list[] = {
 	&ox05b1s_linear_2592x1944_raw10_30fps_4lane,
 	&ox05b1s_linear_2592x1944_raw10_10fps_2lane,
 	&imx415_linear_3480x2160_raw10_60fps_4lane,
+	&sc850sl_linear_3840x2160_raw10_30fps_4lane,
 };
 
 uint32_t vp_get_sensors_list_number() {

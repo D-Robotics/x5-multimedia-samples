b9d8b57125fcacf4ab7e8a4a9687c27a30c5e270
chengyu.he <chengyu.he@horizon.cc>
2025-10-23 11:47:37 +0800

    fix(single_pipe_vin_isp_vse_vpu): [X5-3714] Resolve the issue of fast forward playback of H264 video output
    Change-Id: I3f9a5341fdeeaa5808d7b06fc41910faf5f62138
    Signed-off-by: chengyu.he <chengyu.he@horizon.cc>

--- vp_sensors/vp_sensors.c
+++ vp_sensors/vp_sensors.c
@@ -54,7 +54,7 @@ extern vp_sensor_config_t ov5647_linear_1920x1080_raw10_30fps_2lane;
 extern vp_sensor_config_t ov5647_linear_2592x1944_raw10_15fps_2lane;
 extern vp_sensor_config_t imx477_linear_1280x960_raw10_120fps_2lane;
 extern vp_sensor_config_t imx477_linear_1920x1080_raw12_50fps_2lane;
-extern vp_sensor_config_t imx477_linear_2016x1520_raw12_40fps_2lane;
+extern vp_sensor_config_t imx477_linear_2016x1520_raw12_21fps_2lane;
 extern vp_sensor_config_t imx477_linear_4000x3000_raw12_10fps_2lane;
 extern vp_sensor_config_t ov50h40_linear_4096x3072_raw10_30fps_4lane;
 extern vp_sensor_config_t ox05b1s_linear_2592x1944_raw10_30fps_4lane;
@@ -103,7 +103,7 @@ vp_sensor_config_t *vp_sensor_config_list[] = {
 	&ov5647_linear_2592x1944_raw10_15fps_2lane,
 	&imx477_linear_1280x960_raw10_120fps_2lane,
 	&imx477_linear_1920x1080_raw12_50fps_2lane,
-	&imx477_linear_2016x1520_raw12_40fps_2lane,
+	&imx477_linear_2016x1520_raw12_21fps_2lane,
 	&imx477_linear_4000x3000_raw12_10fps_2lane,
 	&ov50h40_linear_4096x3072_raw10_30fps_4lane,
 	&ox05b1s_linear_2592x1944_raw10_30fps_4lane,

b11a6689a9fd93bad8958d892443b0e29084bc46
xin03liu <xin03liu@d-robotics.cc>
2025-11-07 12:00:13 +0800

    fix(insmode_driver)[RDK-969]: Adapt insmode_driver.sh for RDK OS environment
    Signed-off-by: xin03liu <xin03liu@d-robotics.cc>

--- sample_vot/insmode_driver.sh
+++ sample_vot/insmode_driver.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-modprobe panel-jc-050hd134
-modprobe galcore
-modprobe vio_n2d
-modprobe lontium_lt8618
+systemctl stop lightdm
+
+rmmod vs_drm
+rmmod vs-x5-syscon-bridge
+rmmod sii902x
+rmmod drm_kms_helper
+
+modprobe sii902x
 modprobe vs-x5-syscon-bridge
+modprobe drm_kms_helper
 modprobe vs_drm

807337bc508335fb209db9b205de3a6f9c0acf3d
jinbao.he <jinbao.he@d-robotics.cc>
2025-12-02 11:44:59 +0800

    feat(vp_sensor): [X5-3835] Add sc235hai sensor config
    Summary:
    1. Add sc235hai sensor config to vp sensor list.
    2. Sample can access sc235hai sensor.
    
    Change-Id: I7c7aa4c5d7aa1777b9603d2e1899d38dc5b30fea
    Signed-off-by: jinbao.he <jinbao.he@d-robotics.cc>

--- vp_sensors/vp_sensors.c
+++ vp_sensors/vp_sensors.c
@@ -65,6 +65,7 @@ extern vp_sensor_config_t imx415_linear_3480x2160_raw10_60fps_4lane;
 extern vp_sensor_config_t sc850sl_linear_3840x2160_raw10_30fps_4lane;
 extern vp_sensor_config_t shw3g_linear_2064x1552_raw12_30fps_4lane;
 extern vp_sensor_config_t shw3g_linear_2064x1552_raw12_30fps_4lane_vc1;
+extern vp_sensor_config_t sc235hai_linear_1920x1080_raw10_30fps_2lane;
 
 vp_sensor_config_t *vp_sensor_config_list[] = {
 	&sc1330t_linear_1280x960_raw10_30fps_1lane,
@@ -117,6 +118,7 @@ vp_sensor_config_t *vp_sensor_config_list[] = {
 	&sc850sl_linear_3840x2160_raw10_30fps_4lane,
 	&shw3g_linear_2064x1552_raw12_30fps_4lane,
 	&shw3g_linear_2064x1552_raw12_30fps_4lane_vc1,
+	&sc235hai_linear_1920x1080_raw10_30fps_2lane,
 };
 
 uint32_t vp_get_sensors_list_number() {
